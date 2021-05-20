e8c7a83a8b5e51b35250f00a4c34fdee1667d08c
ActiveAvi <loch.hollows@gmail.com>
2021-05-20 12:18:22 -0400

    hid port number in env var, moved dotenv import

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const port = process.env.PORT || 3000
 
 // middleware
 app.use(cors())
@@ -8,8 +10,7 @@ app.use(express.json())
 
 // routers
 const mailRouter = require('./routers/mail')
-
 app.use(mailRouter)
 
 // server start
-app.listen(3001, () => console.log('server started'))
+app.listen(port, () => console.log(`server started on port ${port}`))
