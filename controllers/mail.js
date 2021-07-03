f27edf3db1d03e5d5dbe14b49d728678c98b5a74
ActiveAvi <loch.hollows@gmail.com>
2021-07-03 17:17:00 -0400

    switched to sendgrid mailer

--- controllers/mail.js
+++ controllers/mail.js
@@ -1,38 +1,24 @@
 module.exports = {
   send: async (req, res) => {
-    let nodemailer = require('nodemailer')
-
     if (!req.body.name || !req.body.message) {
       res.status(400).send('Missing fields')
       return
     }
 
-    const transporter = nodemailer.createTransport({
-      port: 465,
-      host: process.env.EMAIL_HOST,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-      secure: true,
-    })
+    let mailer = require('@sendgrid/mail')
+    mailer.setApiKey(process.env.SENDGRID_API_KEY)
 
     const mailData = {
-      from: process.env.EMAIL_USER,
+      from: 'epsteindidntkillhimself@boomyourballs.social',
       to: process.env.EMAIL_REC,
       subject: `Message from ${req.body.name}`,
       text: `sender: ${req.body.email} message: ${req.body.message}`,
       html: `<div>${req.body.message}</div>`,
     }
 
-    transporter.sendMail(mailData, function (err, info) {
-      if (err) {
-        console.log(err)
-      } else {
-        console.log(info)
-      }
-    })
-
-    res.status(200).send(mailData)
+    mailer
+      .send(mailData)
+      .then(() => res.status(200).send(mailData))
+      .catch((err) => res.status(500).send(err))
   },
 }
